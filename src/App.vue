4604fbfd0f2fd1209b75d20fc873dfaa293c6ee2
masamasasasa <524328307@qq.com>
2022-03-09 19:41:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <ToDoList></ToDoList>
+</template>
+
+<script>
+import ToDoList from './components/todoList/ToDoListComponent.vue'
+export default {
+  name: 'MyApp',
+  components: {
+    ToDoList
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import './assets/bootstrap.css'
+import './assets/todolist.css'
+
+createApp(App).mount('#app')
